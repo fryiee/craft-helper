97bf1dfa827f0ba826602b159995bfa7e5f8b889
Craig Berry <hi@craigberry.co>
2019-08-27 12:12:54 +1000

    update to uid

--- src/Fields.php
+++ src/Fields.php
@@ -160,7 +160,7 @@ class Fields
         }
 
         /** @var FieldRecord $field */
-        $field = self::createField($name, $type, $handle, $settings, 'superTableBlockType:'.$superTableBlockTypeRecord->id, $instructions, $fieldGroupId, $translationMethod, $translationKeyFormat, $runValidation);
+        $field = self::createField($name, $type, $handle, $settings, 'superTableBlockType:'.$superTableBlockTypeRecord->uid, $instructions, $fieldGroupId, $translationMethod, $translationKeyFormat, $runValidation);
 
         if (!$field) {
             return false;
